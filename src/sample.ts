84603de36ad35306050cc87bf81ba42594b05081
kateblomgren <blomgren.kate@gmail.com>
2026-04-02 15:29:49 +0200

    initial update to the template

--- src/sample.ts
+++ src/sample.ts
@@ -1,15 +1,15 @@
 const author = {
-  name: `Angela`,
-  nickname: `angie`,
-  tiktok: `https://www.tiktok.com/@indieteamofone`,
-  ig: `https://www.instagram.com/indieteamofone/`,
-  x: `https://x.com/indieteamofone`,
-  email: "mailto:anthonylannn@gmail.com",
+  name: `Kate`,
+  nickname: `Kate Blomgren`,
+  tiktok: ``,
+  ig: ``,
+  x: ``,
+  email: "mailto:kate.blomgren@liu.se",
 };
 
 const buy = {
-  title: `See on Github`,
-  link: `https://github.com/anthonylan/angie`,
+  title: ``,
+  link: ``,
 };
 
 export { author, buy };
