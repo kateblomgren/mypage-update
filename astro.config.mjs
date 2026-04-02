3486d590e7b12b0215434b7545a62ef8f12778d8
kateblomgren <blomgren.kate@gmail.com>
2026-04-02 15:57:57 +0200

    database update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import vercel from '@astrojs/vercel';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  integrations: [db()]
 });
